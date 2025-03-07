4719d668b89faab208941c6284b3f4c36e711f27
misato kataoka <sakisachi.14@gmail.com>
2025-03-07 23:34:11 +0900

    会員登録画面保存

--- src/app/Http/Requests/ContactRequest.php
+++ src/app/Http/Requests/ContactRequest.php
@@ -13,7 +13,7 @@ class ContactRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,31 @@ class ContactRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+            'weight' => 'required|numeric|digits_between:1,4|regex:/^\d+(\.\d{1})?$/', // 数値で4桁以内かつ小数点1桁まで
+            'target-weight' => 'required|numeric|digits_between:1,4|regex:/^\d+(\.\d{1})?$/', // 数値で4桁以内かつ小数点1桁まで
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => '名前を入力してください',
+            'email.required' => 'メールアドレスを入力してください',
+            'email.email' => 'メールアドレスは「ユーザー名@ドメイン」形式で入力してください',
+            'password.required' => 'パスワードを入力してください',
+            'weight.required' => '現在の体重を入力してください',
+            'weight.numeric' => '現在の体重は数値で入力してください',
+            'weight.digits_between' => '現在の体重は4桁以内で入力してください',
+            'weight.regex' => '現在の体重は小数点1桁で入力してください',
+            'target-weight.required' => '目標体重を入力してください',
+            'target-weight.numeric' => '目標体重は数値で入力してください',
+            'target-weight.digits_between' => '目標体重は4桁以内で入力してください',
+            'target-weight.regex' => '目標体重は小数点1桁で入力してください',
         ];
     }
 }
+
+
--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\WeightLog;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // UserSeederを呼び出す
+        $this->call(UserTableSeeder::class);
+        // WeightLogのダミーデータを35件作成
+        WeightLog::factory()->count(35)->create();
     }
 }
